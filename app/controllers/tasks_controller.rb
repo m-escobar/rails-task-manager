48f252a3f8131f6ceb0b04cd6e0aa41f92c10fb4
Marcelo Escobar <escobar@br.systems>
2019-05-21 12:07:50 -0300

    added task model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+    # def index
+    #     @tasks = Task.all
+    # end
+
+    # def show
+    # end
+
+    # def new
+    # end
+
+    # def create
+    # end
+
+    # def update
+    # end
+
+    # def destroy
+    # end
+end
